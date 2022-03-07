2034639ef928faf733303a033a1964d0aad49723
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-03-07 16:14:33 +0000

    fiz linter issues

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");     
-})
-document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () =>{
-    hamburger.classList.remove("active");
-    hamburger.classList.remove("active");
-
-}))
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  hamburger.classList.remove('active');
+}));
